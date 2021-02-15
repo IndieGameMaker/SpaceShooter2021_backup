36352f91697551f8fdefbb1b4f374fc51f781c37
Lee JaeHyun <myleje@gmail.com>
2021-02-15 14:16:41 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    //접근해야 하는 컴포넌트는 반드시 변수에 할당한 후 사용
+    private Transform tr;
+    Transform transform;
+    //이동 속도 변수(public으로 선언되어 Inspector에 노출됨)
+    public float moveSpeed = 10.0f;
+
     void Start()
     {
-
+        //스크립트가 실행된 후 처음 수행되는 Start 함수에서 Transform 컴포넌트를 할당
+        tr = this.gameObject.GetComponent<Transform>();      
     }
 
     void Update()
@@ -16,5 +23,8 @@ public class PlayerCtrl : MonoBehaviour
 
         Debug.Log("h=" + h);
         Debug.Log("v=" + v);
+
+        //Translate(이동 방향 * 속도 * 변위값 * Time.deltaTime, 기준좌표)
+        tr.Translate(Vector3.forward * moveSpeed * v * Time.deltaTime, Space.Self);
     }
 }
