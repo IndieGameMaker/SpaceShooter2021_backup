4b1131d286302e99c56fabda06aa97faea2859b5
Lee JaeHyun <myleje@gmail.com>
2021-02-16 10:07:37 +0900

    4-9

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -6,8 +6,10 @@ public class PlayerCtrl : MonoBehaviour
 {
     //컴포넌트를 캐시 처리할 변수
     private Transform tr;
-    //이동 속도 변수(public으로 선언되어 인스펙터 뷰에 노출됨)
+    //이동 속력 변수(public으로 선언되어 인스펙터 뷰에 노출됨)
     public float moveSpeed = 10.0f;
+    //회전 속도 변수
+    public float turnSpeed = 80.0f;
 
     void Start()
     {
@@ -19,14 +21,18 @@ public class PlayerCtrl : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");        
 
-        Debug.Log("h=" + h);
-        Debug.Log("v=" + v);
+        // Debug.Log("h=" + h);
+        // Debug.Log("v=" + v);
 
-        //정규화 벡터를 사용한 코드
-        //tr.position += Vector3.forward * 1;
+        //전후좌우 이동 방향 벡터 계산
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
 
-        //Translate 함수를 사용한 이동 로직
-        tr.Translate(Vector3.forward * Time.deltaTime * v * moveSpeed);
+        //Translate(이동 방향 * 속력 * Time.deltaTime)
+        tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime);
+
+        //Vector3.up 축을 기준으로 turnSpeed만큼의 속도로 회전
+        tr.Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
     }
 }
